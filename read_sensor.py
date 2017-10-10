f1ec286ea559c0b5acc4096ca27244c4807a77c6
Pieter Staal <pieterstaal@outlook.com>
2017-10-10 15:44:36 +0200

    Add state retention

--- read_sensor.py
+++ read_sensor.py
@@ -10,18 +10,46 @@ MCAST_PORT = 8845
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 
+print("Callibrating. You have 10 seconds to lay hold the glove still and flat.")
+
+speed = { 'x': 0, 'y': 0, 'z': 0 }
+orientation = { 'x': 0, 'y': 0, 'z': 0 }
+
+for i in range(1, 2):
+    time.sleep(1)
+    print(10 - i)
+
+current_time = time.time()
+
 while True:
-    x = 10
-    y = 15
-    z = 20
-    gyro_data_dict = {'x': x, 'y': y, 'z': z} #sensor.get_gyro_data()
+    dt = current_time - time.time()
+    current_time = time.time()
+
+    # Get data
+    gyro_data_dict = { 'x': 1, 'y': 2, 'z': 3 } #sensor.get_gyro_data()
     gyro_data = str(gyro_data_dict['x']) + " " + \
                 str(gyro_data_dict['y']) + " " + \
                 str(gyro_data_dict['z'])
 
-    accel_data_dict = {'x': x, 'y': y, 'z': z} #sensor.get_accel_data()
+    accel_data_dict = { 'x': 4, 'y': 5, 'z': 6 } #sensor.get_accel_data()
     accel_data = str(accel_data_dict['x']) + " " + \
                  str(accel_data_dict['y']) + " " + \
                  str(accel_data_dict['z'])
-    sock.sendto(accel_data + " " + gyro_data + " " + str(time.time()), (MCAST_GRP, MCAST_PORT))
-    time.sleep(1/60)
+
+    # Update state
+    speed['x'] += accel_data_dict['x'] * dt
+    speed['y'] += accel_data_dict['y'] * dt
+    speed['z'] += accel_data_dict['z'] * dt
+
+    orientation['x'] += gyro_data_dict['x'] * dt
+    orientation['y'] += gyro_data_dict['y'] * dt
+    orientation['z'] += gyro_data_dict['z'] * dt
+
+    speed_data = str(speed['x']) + " " + str(speed['y']) + " " + str(speed['z'])
+    orientation_data = str(orientation['x']) + " " + str(orientation['y'])\
+        + " " + str(orientation['z'])
+
+    sock.sendto(accel_data + " " + gyro_data + " " + \
+        orientation_data + " " + speed_data, (MCAST_GRP, MCAST_PORT))
+
+    time.sleep(1/30.0)
