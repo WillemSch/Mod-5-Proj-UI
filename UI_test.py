d882d459f94a4b1ce1e657309f3de0dcac3b252e
s1856316 <schooltink.willem@gmailcom>
2017-11-02 11:49:33 +0100

    shizzle

---
+++ UI_test.py
@@ -0,0 +1,23 @@
+import socket
+import time
+import random
+
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 8845
+MCAST_SEND_PORT = 8846
+
+send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+send_sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+
+steer_state = 0
+speed_state = 0
+speeds = [-1, -0.3, -0.1, 0, 0.1, 0.3, 1]
+
+while True:
+    steer_state = random.randint(-1, 1)
+
+    data = str(steer_state) + " " + str(speeds[speed_state])
+    print("Trying to send data: steer_state = " + str(steer_state))
+    send_sock.sendto(data.encode(encoding="UTF-8"), (MCAST_GRP, MCAST_SEND_PORT))
+    time.sleep(0.5)
--- ui_v2.py
+++ ui_v2.py
@@ -32,6 +32,8 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', MCAST_PORT))	 # use MCAST_GRP instead of '' to listen only to MCAST_GRP, not all groups on MCAST_PORT
 mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
 
 def minimum(x):
     if x[0] <= x[1]:
@@ -40,8 +42,24 @@ def minimum(x):
         return x[1], x[1]
 
 
+def animate_rotation(prev_angle, target_rotation, delta_time):
+    anim_speed = 0
+    if prev_angle == target_rotation:
+        return prev_angle
+
+    if target_rotation < 180:
+        new_angle = prev_angle + anim_speed * delta_time
+        if new_angle > target_rotation and prev_angle < 180:
+            new_angle = target_rotation
+        elif new_angle > 360:
+            new_angle -= 360
+    else:
+        new_angle = prev_angle - anim_speed * delta_time
+
+
 def calc_pos(x, state):
-    if not state == 0:
+    if not state == 0:  # If the steering state is not 0 the steering wheel is angled, which requires some math to get
+                        # scale right.
         offset = .5 * math.sqrt(math.pow(x[1], 2) + math.pow(x[1], 2)) - .5 * x[1]
     if x[0] <= x[1]:
         difference = x[1]-x[0]
@@ -57,9 +75,10 @@ scale = size
 
 
 def get_steering(state):
+    print("waiting for data")
     data = sock.recv(10240)
     data = data.decode(encoding='UTF-8')
-
+    print("Data decoded")
     return parse_data.parse_steering_state(data)
 
 
@@ -95,10 +114,8 @@ while True:
 
     if steering_state == 1:
         screen.blit(pygame.transform.rotate(temp_wheel, 45), calc_pos(wheel_scale, steering_state))
-        print("state = 1")
-    elif steering_state == 2:
-        screen.blit(pygame.transform.rotate(temp_wheel, -45), calc_pos(wheel_scale, steering_state))
-        print("state = 2")
+    elif steering_state == -1:
+        screen.blit(pygame.transform.rotate(temp_wheel, 315), calc_pos(wheel_scale, steering_state))
     else:
         screen.blit(pygame.transform.rotate(temp_wheel, 0), calc_pos(wheel_scale, steering_state))
 
